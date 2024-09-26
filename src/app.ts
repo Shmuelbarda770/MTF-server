2a943b0768bda1cf94121bc1a0bd224ff0a8af12
shmuel <shmuelbarda770@gmail.com>
2024-09-26 17:03:15 +0300

    my first check

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+import express, { Request, Response } from 'express';
+import SwaggerUI from 'swagger-ui-express';
+import dotenv from 'dotenv';
+const swagger  = require('../swagger.json')
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/swagger', SwaggerUI.serve, SwaggerUI.setup(swagger));
+
+app.get('/users', (req: Request, res: Response) => {
+    const data : ApiResponse   = {
+        isSuccessful:true,
+        displayMessage:null,
+        description:null,
+        exception:null,
+        timestamp:null,
+        data:{}
+        
+    }
+  res.json(data);
+});  
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
---
+++ types/types.d.ts
@@ -0,0 +1,17 @@
+interface ApiResponse {
+    isSuccessful: boolean;
+    displayMessage: string | null;
+    description: string | null;
+    exception: string | null;
+    timestamp: string | null;
+    data?: object;
+  }
+
+ declare namespace NodeJS {
+    interface ProcessEnv {
+      PORT : string;
+      DB_NAME: string;
+      DB_USER_NAME:string;
+      DB_PASSWORD :string;
+    }
+  }  

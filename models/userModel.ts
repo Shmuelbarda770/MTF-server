dd432a1341e2c227772bb0e11596ffe356b80ddd
israel <hartl123452019@gmail.com>
2024-09-29 13:25:05 +0300

    feat: add mongoose and create user model for database schema

---
+++ models/userModel.ts
@@ -0,0 +1,15 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface User extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<User>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+export const User = mongoose.model<User>('User', userSchema);

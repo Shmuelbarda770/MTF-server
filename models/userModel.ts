27dd5a22e9ebc57ec89cb42662e39707ceecd8cb
israel <hartl123452019@gmail.com>
2024-09-30 10:13:00 +0300

    change the deseription

--- models/userModel.ts
+++ models/userModel.ts
@@ -1,9 +1,17 @@
 import { Schema, model } from 'mongoose';
 
-// Define the User schema
+// Define the User schema 
+// The user have: id (ת"ז), Role (תפקיד),
+// FirstName (שם פרטי), LastName(שם משפחה),
+// PhoneNumber (מספר טלפון), Email (אימייל),
+// AccountName (שם חשבון),CompanyName (שם חברה),
+// need to create a Role schema  separately?
+// need to add anther attribute?
+// need to change a type for one of the  
+
 const userSchema = new Schema({
   Id: { type: Number, required: true },
-  role: { type: String, required: true, enum: ['Owner'] },
+  Role: { type: String, required: true, enum: ['Owner'] },
   FirstName: { type: String, required: true },
   LastName: { type: String, required: true },
   PhoneNumber: { type: String, required: true },

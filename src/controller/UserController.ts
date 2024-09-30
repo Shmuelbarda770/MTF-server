ffed1c37ec1739b1aaf3ecf51fade4c274b86d92
israel <israeledrey770@gmail.com>
2024-09-30 10:29:28 +0300

    Committing local changes to UserController.ts

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,55 +1,22 @@
 import { Request, Response } from 'express';
-import { createApiResponse, ApiResponse } from '../util/ApiResponse';
-import { getCollection, connect, close } from '../util/Mongo';
+import {createApiResponse} from '../util/ApiResponse'
 
+import {getCollection,connect, close} from '../util/Mongo'
+import { log } from 'console';
 
-export const createUser:any = async (req: Request, res: Response) => {
-    const userData = req.body;
-
-    try {
-        await connect();
-        const users = getCollection("users");
-
-       
-        const existingUser = await users.findOne({ email: userData.email });
-        const errors: any = validateUser(userData, existingUser);
+export const createUser = async (req: Request, res: Response) => {
+    try{
+         await connect();
+        const users=getCollection("users");
+        const userData = req.body;
         
-        if (errors.length > 0) {
-            return res.status(400).json(createApiResponse(false, null, "Validation errors", null, errors));
-        }
-
-        await users.insertOne(userData);
+   
         const response: ApiResponse = createApiResponse(true, userData, "User created");
         res.status(201).json(response);
 
-    } catch (error: any) {
-        console.log(error);
-        const response: ApiResponse = createApiResponse(false, null, "User creation failed", null, error.message);
-        res.status(500).json(response); 
-    } finally {
+    }catch(error){
+        console.log(error)
+    }finally {
         await close(); 
     }
-};
-
-
-const validateUser = (userData: any, existingUser: any) => {
-    const errors = [];
-    
-    if (existingUser) {
-        errors.push("User already exists");
-    }
-    if (!userData.firstName) {
-        errors.push("First name is required");
-    }
-    if (!userData.lastName) {
-        errors.push("Last name is required");
-    }
-    if (!userData.role) {
-        errors.push("Role is required");
-    }
-    if (!userData.email || !/\S+@\S+\.\S+/.test(userData.email)) {
-        errors.push("Valid email is required");
-    }
-
-    return errors;
 };

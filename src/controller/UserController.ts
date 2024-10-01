683574347d6b5777997013f2f0a581009b533c10
israel <israeledrey770@gmail.com>
2024-10-01 16:05:00 +0300

    add delete function

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -3,15 +3,20 @@ import { createApiResponse, ApiResponse } from '../util/ApiResponse';
 import { getCollection, connect, close } from '../util/Mongo';
 import mongoose from 'mongoose';
 import User from '../models/userModel';
-export const createUser :any= async (req: Request, res: Response) => {
+
+
+
+
+export const createUser: any = async (req: Request, res: Response) => {
+
     const userData = req.body;
 
     try {
         await connect();
         const users = getCollection("users");
-        const existingUser = await users.findOne({ email: userData.email});
+        const existingUser = await users.findOne({ email: userData.email });
         // const errors: any = validateUser(userData, existingUser);
-        
+
         // if (errors.length > 0) {
         //     return res.status(400).json(createApiResponse(false, null, "Validation errors", null, errors));
         // }
@@ -23,9 +28,9 @@ export const createUser :any= async (req: Request, res: Response) => {
     } catch (error: any) {
         console.log(error);
         const response: ApiResponse = createApiResponse(false, null, "User creation failed", null, error.message);
-        res.status(500).json(response); 
+        res.status(500).json(response);
     } finally {
-        await close(); 
+        await close();
     }
 };
 
@@ -35,21 +40,21 @@ export const getAllUsers = async (req: Request, res: Response) => {
     try {
         await mongoose.connect('');
         const users = await User.find();
-        if(users.length>0){
+        if (users.length > 0) {
             const response: ApiResponse = createApiResponse(true, users, "get all users from db", null, null);
             res.status(200).json(response);
-        }else{
+        } else {
             const response: ApiResponse = createApiResponse(true, users, "get all users from db failed no have users", null, null);
             res.status(400).json(response);
         }
-       
+
 
     } catch (error: any) {
         console.log(error);
         const response: ApiResponse = createApiResponse(false, null, "get all users from db failed", null, error.message);
         res.status(500).json(response);
     } finally {
-        await mongoose.connection.close(); 
+        await mongoose.connection.close();
     }
 };
 
@@ -63,15 +68,15 @@ export const searchInput = async (req: Request, res: Response) => {
     let users;
 
     try {
-        await mongoose.connect(''); 
+        await mongoose.connect('');
 
-        
+
         if (searchUsers.trim() === '') {
             // users = await User.find();
         } else if (searchUsers.length > 1) {
-            users = await User.find({ username: { $regex: searchUsers, $options: 'i' } }); 
+            users = await User.find({ username: { $regex: searchUsers, $options: 'i' } });
         } else {
-            users = await User.find({ username: searchUsers }); 
+            users = await User.find({ username: searchUsers });
         }
 
         const response: ApiResponse = createApiResponse(true, users, "Search results", null, null);
@@ -82,7 +87,29 @@ export const searchInput = async (req: Request, res: Response) => {
         const response: ApiResponse = createApiResponse(false, null, "Failed to retrieve users", null, error.message);
         res.status(500).json(response);
     } finally {
-        await mongoose.connection.close(); 
+        await mongoose.connection.close();
     }
 };
+
+export const deleteUser: any = async (req: Request, res: Response) => {
+
+    const { email } = req.body;
+    try {
+        await connect();
+        const user = await User.findOne({ email ““: email })
+        if (!user) {
+            return res.status(404).json(createApiResponse(false, null, "User not found", null, null));
+        }
+        await User.deleteOne({ email })
+        const response: ApiResponse = createApiResponse(true, { email }, "User deleted");
+        res.status(200).json(response);
+
+    } catch (error: any) {
+        const response: ApiResponse = createApiResponse(false, null, "user not deleted", null, error.message);
+        res.status(500).json(response);
+
+    } finally {
+        await close();
+    }
+}
 
--- src/routes/UserRoute.ts
+++ src/routes/UserRoute.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createUser,getAllUsers,searchInput} from '../controller/UserController'; 
+import {createUser,getAllUsers,searchInput, deleteUser} from '../controller/UserController'; 
 
 const router = express.Router();
 
 router.post('/api/users', createUser);
 router.get('/users/',getAllUsers)
 router.get('/users/searchUsers', searchInput)
+router.delete('/deleteUser', deleteUser)
 
 export default router;

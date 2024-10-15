4c0963d673c0758e8d142471bb426a6444abb61e
William Justice Davis <hvusvztc770@gmail.com>
2024-10-15 12:10:12 +0300

    fixed duplicate and missing import

--- src/controller/SitesController.ts
+++ src/controller/SitesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { connect } from '../util/Mongo';
 import { createApiResponse } from '../util/ApiResponse';
 import Site from '../models/siteModel'; // Assuming the model is in the models folder
+import { ApiResponse } from '../util/ApiResponse';
 
 // Controller function to delete a site by its ID
 export const deleteSite = async (req: Request, res: Response): Promise<void> => {
@@ -18,25 +19,6 @@ export const deleteSite = async (req: Request, res: Response): Promise<void> => 
   } catch (error) {
     res.status(500).json({ message: 'Error deleting site', error });
   }
-};
-
-export const getAllSites = async (req: Request, res: Response) => {
-  try {
-    await connect(); // חיבור למונגו
-    const sites = await Site.find(); // שאיבת כל אתרי ה-Site מהמאגר
-    if (sites.length > 0) {
-      const response = createApiResponse(true, sites, "Fetched all sites successfully", null, null);
-      res.status(200).json(response);
-    } else {
-      const response = createApiResponse(true,sites,"No sites found",null,null);
-      res.status(404).json(response); // סטטוס 404 אם לא נמצאו אתרים
-    }
-  } catch (error: any) {
-    console.error('Error fetching sites:', error);
-    const response = createApiResponse(false,null,"Failed to fetch sites",null,error.message);
-    res.status(500).json(response);
-      
-  }    
 };
 
 // This function creates a site in the db

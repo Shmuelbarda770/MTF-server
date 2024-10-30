2fadabfff3ea027b1e877d9434319d7a0b6912c7
israel pinto <hartl123452019@gmaul.com>
2024-10-30 07:42:40 +0200

    merge with dev

--- src/routes/SiteRoute.ts
+++ src/routes/SiteRoute.ts
@@ -1,7 +1,7 @@
 
 
 import express from 'express';
-import { deleteSite, getAllSites, searchSites, createSite, getSiteById, updateSide , exportSiteList } from '../controller/SitesController';
+import { deleteSite, getAllSites, searchSites, createSite, getSiteById, updateSide, exportSiteList } from '../controller/SitesController';
 
 const router = express.Router();
 
@@ -11,5 +11,5 @@ router.post('/searchSites', searchSites);
 router.post('/createSite', createSite);
 router.get('/getSite/:id', getSiteById);
 router.patch('/updateSide/:id', updateSide);
-router.get('/export-sites',  exportSiteList);
+router.get('/export-sites', exportSiteList);
 export default router;
